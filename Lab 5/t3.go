c7533a07e32526fa009f4d6fc5efb6ae1d99206f
Junde Yhi <lmy441900@aosc.xyz>
2017-10-27 12:43:54 +0800

    Lab 5/t3.go: fix buffered I/O issue

--- Lab 5/t3.go
+++ Lab 5/t3.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "os"
+  "bufio"
 )
 
 const XRate_US2EUR float64 = 0.5
@@ -12,23 +13,42 @@ func main() {
   os.Exit(t3_main())
 }
 
+// NOTE: None of Stdin, Stdout or Stderr are buffered. See:
+// http://grokbase.com/t/gg/golang-nuts/158cgb3rfd/go-nuts-os-stdout-is-not-buffered:
+//
+// > In C, printf, fprintf functions write to a FILE *stream, and "stdout" is a
+// > "stream", not a file.
+// >
+// > In Go, fmt.Print, fmt.Fprintf functions write directly to a file, not a
+// > Writer (equivalent to a C "stream").
+// >
+// > It is just that the "stdout" word means different things in C and Go ...
+//
+// This means, to achieve the same behavior as C's buffered "streams", we must
+// by hand create an I/O buffer.
+
 func t3_main() int {
   var usd, result float64 = 0, 0
   var selection rune = 0
   var err error
 
+  // In this case we only create a buffer for Stdin.
+  var stdin *bufio.Reader = bufio.NewReader(os.Stdin);
+
   fmt.Println("Please input the amount of US dollars:")
-  _, err = fmt.Scanf("%f", &usd)
+  // _, err = fmt.Scanf("%f", &usd)
+  _, err = fmt.Fscanf(stdin, "%f", &usd)
   if err != nil {
     fmt.Println(err, ": Illegal number of US dollars.")
     return 255
   }
 
   // this stdin very dirty
-  // but how to clean it?
+  _, _ = stdin.Discard(stdin.Buffered())
 
   fmt.Println("Convert to Euro(E) or RMB(R)?")
-  _, err = fmt.Scanf("%c", &selection)
+  // _, err = fmt.Scanf("%c", &selection)
+  _, err = fmt.Fscanf(stdin, "%c", &selection)
   if err != nil {
     fmt.Println(err, ": Illegal type of conversion.")
     return 255
@@ -52,7 +72,8 @@ func t3_main() int {
   fmt.Printf("%g US dollars converts to %g %s.\n", usd, result, currency_name(selection))
 
   // NOTE: Golang's os.Exit does NOT even flush stdin.
-  // Should flush it right before we return (and exit). But how (elegantly)?
+  // Should flush it right before we return (and exit).
+  _, _ = stdin.Discard(stdin.Buffered())
 
   return 0
 }
