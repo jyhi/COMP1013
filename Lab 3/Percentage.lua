cbf9750b5ad9532275d46f2ee73862f73fb82545
Junde Yhi <lmy441900@aosc.xyz>
2018-02-22 19:07:34 +0800

    Lab 3/Percentage.lua: implement successfully

--- Lab 3/Percentage.lua
+++ Lab 3/Percentage.lua
@@ -1,1 +1,15 @@
--- Given up.
+local buf = io.read()
+
+-- XXX: This is tricky.
+local buf_iter_num = string.gmatch(buf, "%d+")
+local buf_iter_char = string.gmatch(buf, "%a")
+
+local percentage = tonumber(buf_iter_num())
+local ch1 = buf_iter_char() -- NOTE: This is a string
+local ch2 = buf_iter_char() -- NOTE: This is a string
+local x = tonumber(buf_iter_num())
+
+io.write(string.format(
+  "%d%% %s%s %d is %f\n",
+  percentage, ch1, ch2, x, 1.0 * x * percentage / 100
+))
